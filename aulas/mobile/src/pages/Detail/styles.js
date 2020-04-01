75c74610b15ea12dcb19bd2347dc9cd80ab22569
Emilene Goulart <emigoulart@gmail.com>
2020-03-31 22:32:35 -0300

    Detail page changes, mobile.

---
+++ aulas/mobile/src/pages/Detail/styles.js
@@ -0,0 +1,122 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants'
+ export default StyleSheet.create({
+  container: {
+      flex : 1,
+      paddingHorizontal:24,
+      paddingTop:Constants.statusBarHeight + 20,
+  } ,
+  
+  header:{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+
+  },
+
+  headerText:{
+      fontSize: 15,
+      color:'#737380',
+  },
+
+  headerTextBold:{
+      fontWeight:'bold'
+  },
+
+  title:{
+      fontSize:30,
+      marginBottom:16,
+      marginTop:48,
+      color:'#13131a',
+      fontWeight:'bold'
+  },
+
+   description: {
+    fontSize:16,
+    lineHeight:24,
+    color:'#737380'
+
+   },
+
+   incidentList: {
+       marginTop:32,
+   },
+
+   incident: {
+       padding: 24,
+       borderRadius: 8,
+       backgroundColor:'#FFF',
+       marginBottom: 16,
+       marginTop:48,
+   },
+
+   incidentProperty:{
+       fontSize: 14,
+       color: '#41414d',
+       fontWeight: 'bold',
+       marginTop:24,
+   },
+
+   incidentValue:{
+    marginTop:8,
+    fontSize:15,
+    marginBottom: 24,
+    color:'#737380' 
+   },
+
+   detailsButton:{
+       flexDirection: 'row',
+       justifyContent: "space-between",
+       alignItems: 'center'
+   },
+
+   detailsButtonText:{
+       color:'#E02041',
+       fontSize: 15,
+       fontWeight: 'bold'
+   },
+
+   contactBox: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor:'#FFF',
+    marginBottom: 16,
+    marginTop:48,
+},
+
+heroTitle: {
+    fontWeight: 'bold',
+    fontSize:20,
+    lineHeight:30,
+    color:'#13131a'
+
+   },
+
+   heroDescription: {
+    fontSize:15,
+    lineHeight:30,
+    color:'#737380',
+    marginTop: 16,
+},
+
+actions:{
+    marginTop: 16,
+    flexDirection:'row',
+    justifyContent:'space-between',
+},
+   action:{
+       backgroundColor: '#E02041',
+       borderRadius:8,
+       height:50,
+       width:'48%',
+       justifyContent: 'center',
+       alignItems:'center'
+   },
+
+   actionText:{
+       color: '#FFF',
+       fontSize:15,
+       fontWeight:'bold'
+   },
+
+ });
